90e2339f8f64fb19aaba5afb107a859b5744de78
joosnlaurence <jasongotanl@gmail.com>
2024-11-09 21:43:19 -0500

    Add instruction_partition and sign_extend

--- project.c
+++ project.c
@@ -5,14 +5,57 @@
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
-    
+    //add
+	if (ALUControl == 0) // 000
+		*ALUresult = A + B;
+
+	//subtract
+	else if (ALUControl == 1) // 001
+		*ALUresult = A - B;
+
+	//SLT (set if less than) for signed integers
+	else if (ALUControl == 2) // 010
+		*ALUresult = ((int)A < (int)B) ? 1 : 0;
+	
+	//SLT for unsigned integers
+	else if (ALUControl == 3) // 011
+		*ALUresult = (A < B) ? 1 : 0;
+
+	//AND
+	else if (ALUControl == 4) // 100
+		*ALUresult = A & B;
+
+	//OR
+	else if (ALUControl == 5) // 101
+		*ALUresult = A | B;
+
+	//Shift Left by 16 
+	else if (ALUControl == 6) // 110
+		*ALUresult = B << 16;
+
+	//NOT of A
+	else // 111
+		*ALUresult = ~A;
+
+	//Assign Zero to 1 if the result is zero; otherwise, assign 0.
+	*Zero = (*ALUresult == 0) ? 1 : 0;
 }
 
 /* instruction fetch */
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
+    // Check if PC is word-aligned
+	if(PC % 4 != 0) 
+		return 1;
 
+	// Fetch instruction from memory
+	*instruction = Mem[(PC >> 2)];
+
+	if (*instruction == 0x00000000)
+		return 1;
+
+	return 0;
 }
 
 
@@ -20,7 +63,21 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
+    unsigned op_mask = 0xFC000000; // instruction [31-26]
+    unsigned r1_mask = 0x03E00000; // instruction [25-21]  
+    unsigned r2_mask = r1_mask >> 5; // instruction [20-16]
+    unsigned r3_mask = r2_mask >> 5; // instruction [15-11]
+    unsigned funct_mask = 0x0000003F; // instruction [5-0]
+    unsigned offset_mask = 0x0000FFFF; // instruction [15-0]
+    unsigned jsec_mask = ~op_mask;  // instruction [25-0]      
 
+    *op = (instruction & op_mask) >> 26;              
+    *r1 = (instruction & r1_mask) >> 21;    
+    *r2 = (instruction & r2_mask) >> 16;    
+    *r3 = (instruction & r3_mask) >> 11;    
+    *funct = instruction & funct_mask;
+    *offset = instruction & offset_mask; 
+    *jsec = instruction & jsec_mask;   
 }
 
 
@@ -43,7 +100,10 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-
+    if((offset >> 15) == 1)
+        offset = offset | 0xFFFF0000;
+    
+    *extended_value = offset;
 }
 
 /* ALU operations */
