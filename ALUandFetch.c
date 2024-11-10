2612d69d09e3a3816260938ed64e0f3416e038b8
joosnlaurence <jasongotanl@gmail.com>
2024-11-09 21:02:10 -0500

    Fix bugs and add word alignment check to PC

--- ALUandFetch.c
+++ ALUandFetch.c
@@ -6,35 +6,35 @@
 void ALU(unsigned A, unsigned B, char ALUControl, unsigned* ALUresult, char* Zero)
 {
 	//add
-	if (ALUControl == '000')
+	if (ALUControl == 0) // 000
 		*ALUresult = A + B;
 
 	//subtract
-	else if (ALUControl == '001')
+	else if (ALUControl == 1) // 001
 		*ALUresult = A - B;
 
 	//SLT (set if less than) for signed integers
-	else if (ALUControl == '010')
+	else if (ALUControl == 2) // 010
 		*ALUresult = ((int)A < (int)B) ? 1 : 0;
 	
 	//SLT for unsigned integers
-	else if (ALUControl == '011') 		
+	else if (ALUControl == 3) // 011
 		*ALUresult = (A < B) ? 1 : 0;
 
 	//AND
-	else if (ALUControl == '100')
+	else if (ALUControl == 4) // 100
 		*ALUresult = A & B;
 
 	//OR
-	else if (ALUControl == '101')
+	else if (ALUControl == 5) // 101
 		*ALUresult = A | B;
 
-	//Shift Left by 16
-	else if (ALUControl == '110')
+	//Shift Left by 16 
+	else if (ALUControl == 6) // 110
 		*ALUresult = B << 16;
 
 	//NOT of A
-	else (ALUControl == '111')
+	else // 111
 		*ALUresult = ~A;
 
 	//Assign Zero to 1 if the result is zero; otherwise, assign 0.
@@ -45,8 +45,10 @@ void ALU(unsigned A, unsigned B, char ALUControl, unsigned* ALUresult, char* Zer
 /* 10 Points */
 int instruction_fetch(unsigned PC, unsigned* Mem, unsigned* instruction)
 {
-
 	// Fetch instruction from memory
+	if(PC % 4 != 0) 
+		return 1;
+		
 	*instruction = Mem[(PC >> 2)];
 
 	if (*instruction == 0x00000000)
